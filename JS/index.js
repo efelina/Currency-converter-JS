4e89bc98866398bc8981d5ceefbdc87fa27c9003
Ewelina Herec <ewe.herec@gmail.com>
2020-08-18 21:54:29 +0200

    adding change in indentation in code

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,4 @@
-
-
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     currencyForm = document.getElementById('currencyForm');
     inputCurrency = document.getElementById('inputCurrency');
     amount = document.getElementById('amount')
@@ -12,48 +10,45 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 
     currencyForm.addEventListener('submit', (event) => {
-        event.preventDefault();  
+        event.preventDefault();
         let valueToConvert = event.target.elements[0].value;
         let currencyCodeId = event.target.elements[1].value;
-        logo.className="logoChange";              
+        logo.className = "logoChange";
         let code = "";
-        switch (currencyCodeId){
-        case "1":
-            code = "EUR";
-            break;
-        case "2":
-           code = "USD";
-           break;
-        case "3":
-           code = "CHF";
-           break;
+        switch (currencyCodeId) {
+            case "1":
+                code = "EUR";
+                break;
+            case "2":
+                code = "USD";
+                break;
+            case "3":
+                code = "CHF";
+                break;
         }
-               
-        getExchangeRate(code).then(data => {         
-        let exchangeRate = data.rates[0].mid;       
-        exchangeCurrency(valueToConvert, exchangeRate); 
-        //event.target.elements[0].value=''
-                       
-        }) 
-                 
-       setTimeout(function(){
-            logo.className="logo";            
-        },4000)
-       
+
+        getExchangeRate(code).then(data => {
+            let exchangeRate = data.rates[0].mid;
+            exchangeCurrency(valueToConvert, exchangeRate);
+        })
+
+        setTimeout(function () {
+            logo.className = "logo";
+        }, 4000)
+
     })
-   
+
 })
 
 const getExchangeRate = (code) => {
     return fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${code}`)
-    .then(resp => resp.json())
-    .then(data => data)
-    
+        .then(resp => resp.json())
+        .then(data => data)
 }
 
-    
-function exchangeCurrency(valueToConvert, exchangeRate){  
-    let exchange = (valueToConvert*exchangeRate).toFixed(2);    
-    amount.innerText = `${exchange} PLN`;    
+
+function exchangeCurrency(valueToConvert, exchangeRate) {
+    let exchange = (valueToConvert * exchangeRate).toFixed(2);
+    amount.innerText = `${exchange} PLN`;
 }
 
